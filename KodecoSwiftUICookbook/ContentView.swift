c780a1347f20a454d289147dbfcf4a4a8fb97011
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2024-05-12 08:34:47 -0700

    Initial Commit

---
+++ KodecoSwiftUICookbook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KodecoSwiftUICookbook
+//
+//  Created by Tatsuya Moriguchi on 5/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KodecoSwiftUICookbook/KodecoSwiftUICookbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  KodecoSwiftUICookbookApp.swift
+//  KodecoSwiftUICookbook
+//
+//  Created by Tatsuya Moriguchi on 5/12/24.
+//
+
+import SwiftUI
+
+@main
+struct KodecoSwiftUICookbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
