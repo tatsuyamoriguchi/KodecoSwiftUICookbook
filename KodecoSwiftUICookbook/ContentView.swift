8b8ba84a8e39f002e692fce35752248e50109460
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2024-05-14 10:06:14 -0700

    Add a NavigationStack in SwiftUI, Add a List View in SwiftUI

--- KodecoSwiftUICookbook/ContentView.swift
+++ KodecoSwiftUICookbook/ContentView.swift
@@ -8,14 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
+        
+        NavigationStack{
+            
+            VStack {
+                Text("Hello, SwiftUI")
+                NavigationLink(destination: DetailView()) {
+                    Text("Go to Detail View")
+                }
+            }
+            .navigationTitle("Home")
+            .navigationBarTitleDisplayMode(.large)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        print("Settings pressed")
+                    }) {
+                        Text("Settings")
+                    }
+                }
+            }
+            
+            Button(action: {
+                print("Button tapped")
+            }) {
+                Text("Tap me.")
+                    .font(.largeTitle)
+                    .foregroundStyle(Color.white)
+            }
+            .padding()
+            .background(
+                LinearGradient(gradient: Gradient(colors: [.purple, .pink]), startPoint: .topLeading, endPoint: .bottomTrailing))
+            .cornerRadius(10)
+            
+            Image("Kodeco")
+                .resizable()
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 300, height: 300)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.black, lineWidth: 4))
+                .shadow(radius:10)
+            
+            
+            
+            Text("Welcome to Kodeco SwiftUI Cookbook.")
         }
         .padding()
+    }
+}
+
+
+struct DetailView: View {
+    let tasks = ["Design Login UI", "Login view UI code", "Authentication code", "Keychain code"]
+    
+    var body: some View {
+        Text("This is the detail view.")
+            .navigationTitle("Detail")
+            .navigationBarTitleDisplayMode(.large)
+        List(tasks, id: \.self) { task in
+            Text(task)
+        }
     }
 }
 
