927133a3605d9f344fe158cc6dd35393c5b5abbc
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2024-05-16 09:14:47 -0700

    Add Shadows to Views in SwiftUI Clip Views in SwiftUI

---
+++ KodecoSwiftUICookbook/MoreSwiftUI.swift
@@ -0,0 +1,18 @@
+//
+//  MoreSwiftUI.swift
+//  KodecoSwiftUICookbook
+//
+//  Created by Tatsuya Moriguchi on 5/16/24.
+//
+
+import SwiftUI
+
+struct MoreSwiftUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MoreSwiftUI()
+}
