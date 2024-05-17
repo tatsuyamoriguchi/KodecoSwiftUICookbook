07ea2d5fc391fcb869c6a92325f1c8086c24fa6c
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2024-05-17 10:20:21 -0700

    Adjust View Opacity in SwiftUI Create a Circular View in SwiftUI Build a Grid of Views in SwiftUI

---
+++ KodecoSwiftUICookbook/Grid.swift
@@ -0,0 +1,18 @@
+//
+//  Grid.swift
+//  KodecoSwiftUICookbook
+//
+//  Created by Tatsuya Moriguchi on 5/17/24.
+//
+
+import SwiftUI
+
+struct Grid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Grid()
+}
