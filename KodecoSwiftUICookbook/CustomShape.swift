afed3b39a83c3d04ad6380385aab5423e2fdb442
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2024-05-18 15:40:54 -0700

    custom modifier and custom shape

---
+++ KodecoSwiftUICookbook/CustomShape.swift
@@ -0,0 +1,18 @@
+//
+//  CustomShape.swift
+//  KodecoSwiftUICookbook
+//
+//  Created by Tatsuya Moriguchi on 5/18/24.
+//
+
+import SwiftUI
+
+struct CustomShape: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomShape()
+}
---
+++ KodecoSwiftUICookbook/CustomViewModifier.swift
@@ -0,0 +1,18 @@
+//
+//  CustomViewModifier.swift
+//  KodecoSwiftUICookbook
+//
+//  Created by Tatsuya Moriguchi on 5/18/24.
+//
+
+import SwiftUI
+
+struct CustomViewModifier: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomViewModifier()
+}
