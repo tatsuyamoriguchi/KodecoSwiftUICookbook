08410f2eb7f05c56d1d51a7d0762bb0577fa5371
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2024-05-20 11:04:43 -0700

    Understanding ObservableObject & ObservedObject Sharing State Across Views With Environment Objects

---
+++ KodecoSwiftUICookbook/Environment Objects.swift
@@ -0,0 +1,18 @@
+//
+//  Environment Objects.swift
+//  KodecoSwiftUICookbook
+//
+//  Created by Tatsuya Moriguchi on 5/20/24.
+//
+
+import SwiftUI
+
+struct Environment_Objects: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Environment_Objects()
+}
---
+++ KodecoSwiftUICookbook/ObservableObject & ObservedObject.swift
@@ -0,0 +1,18 @@
+//
+//  ObservableObject & ObservedObject.swift
+//  KodecoSwiftUICookbook
+//
+//  Created by Tatsuya Moriguchi on 5/20/24.
+//
+
+import SwiftUI
+
+struct ObservableObject___ObservedObject: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ObservableObject___ObservedObject()
+}
