de140f09e200bfe67b8c773d8ae6f4f3ca2b98fe
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2024-05-21 09:59:33 -0700

    Managing Observable Objects With StateObject

---
+++ KodecoSwiftUICookbook/StateObject.swift
@@ -0,0 +1,18 @@
+//
+//  StateObject.swift
+//  KodecoSwiftUICookbook
+//
+//  Created by Tatsuya Moriguchi on 5/21/24.
+//
+
+import SwiftUI
+
+struct StateObject: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StateObject()
+}
