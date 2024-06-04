935c56139d10073b026eceea516a2d2b1e2d1061
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2024-06-03 23:58:25 -0700

    SF Symbols

---
+++ KodecoSwiftUICookbook/SFSymbols.swift
@@ -0,0 +1,18 @@
+//
+//  SFSymbols.swift
+//  KodecoSwiftUICookbook
+//
+//  Created by Tatsuya Moriguchi on 6/3/24.
+//
+
+import SwiftUI
+
+struct SFSymbols: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SFSymbols()
+}
