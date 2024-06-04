e26d027c8976c6919ba9bc84b544bc444e3612d5
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2024-06-04 10:57:26 -0700

    Custom image shape

---
+++ KodecoSwiftUICookbook/CustomImageShape.swift
@@ -0,0 +1,18 @@
+//
+//  CustomImageShape.swift
+//  KodecoSwiftUICookbook
+//
+//  Created by Tatsuya Moriguchi on 6/4/24.
+//
+
+import SwiftUI
+
+struct CustomImageShape: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomImageShape()
+}
