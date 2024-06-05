7d90b387450b156e2a8579c65efa716e13aae5d6
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2024-06-05 10:12:33 -0700

    Adding a shadow and filter to an Image

---
+++ KodecoSwiftUICookbook/FilterToImage.swift
@@ -0,0 +1,18 @@
+//
+//  FilterToImage.swift
+//  KodecoSwiftUICookbook
+//
+//  Created by Tatsuya Moriguchi on 6/5/24.
+//
+
+import SwiftUI
+
+struct FilterToImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FilterToImage()
+}
---
+++ KodecoSwiftUICookbook/ShadowToImage.swift
@@ -0,0 +1,18 @@
+//
+//  ShadowToImage.swift
+//  KodecoSwiftUICookbook
+//
+//  Created by Tatsuya Moriguchi on 6/5/24.
+//
+
+import SwiftUI
+
+struct ShadowToImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShadowToImage()
+}
