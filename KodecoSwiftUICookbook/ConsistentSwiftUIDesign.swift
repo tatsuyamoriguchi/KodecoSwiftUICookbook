66a0a58bd164af66c40994652b98e11b24462791
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2024-06-07 11:14:52 -0700

    Button

---
+++ KodecoSwiftUICookbook/ConsistentSwiftUIDesign.swift
@@ -0,0 +1,18 @@
+//
+//  ConsistentSwiftUIDesign.swift
+//  KodecoSwiftUICookbook
+//
+//  Created by Tatsuya Moriguchi on 6/7/24.
+//
+
+import SwiftUI
+
+struct ConsistentSwiftUIDesign: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ConsistentSwiftUIDesign()
+}
---
+++ KodecoSwiftUICookbook/CreateButton.swift
@@ -0,0 +1,18 @@
+//
+//  CreateButton.swift
+//  KodecoSwiftUICookbook
+//
+//  Created by Tatsuya Moriguchi on 6/7/24.
+//
+
+import SwiftUI
+
+struct CreateButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateButton()
+}
