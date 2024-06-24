beb0b93111014503c7663f9f478b243b2620b866
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2024-06-24 09:05:29 -0700

    Sequencing Gestures *Rotation is not working

---
+++ KodecoSwiftUICookbook/SequencingGestures.swift
@@ -0,0 +1,18 @@
+//
+//  SequencingGestures.swift
+//  KodecoSwiftUICookbook
+//
+//  Created by Tatsuya Moriguchi on 6/24/24.
+//
+
+import SwiftUI
+
+struct SequencingGestures: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SequencingGestures()
+}
