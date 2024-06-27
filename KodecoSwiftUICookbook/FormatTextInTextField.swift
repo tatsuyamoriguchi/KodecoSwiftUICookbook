ea14d9353227343d74ac2f5be7c2a80e1a400645
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2024-06-27 08:54:14 -0700

    Format Text in TextField Style TextField

---
+++ KodecoSwiftUICookbook/FormatTextInTextField.swift
@@ -0,0 +1,18 @@
+//
+//  FormatTextInTextField.swift
+//  KodecoSwiftUICookbook
+//
+//  Created by Tatsuya Moriguchi on 6/27/24.
+//
+
+import SwiftUI
+
+struct FormatTextInTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FormatTextInTextField()
+}
---
+++ KodecoSwiftUICookbook/StyleTextField.swift
@@ -0,0 +1,18 @@
+//
+//  StyleTextField.swift
+//  KodecoSwiftUICookbook
+//
+//  Created by Tatsuya Moriguchi on 6/27/24.
+//
+
+import SwiftUI
+
+struct StyleTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StyleTextField()
+}
