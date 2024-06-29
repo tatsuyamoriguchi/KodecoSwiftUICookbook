bcf20bf1c43678f8cdaf882b802ebd0f7a9d5fca
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2024-06-29 13:40:04 -0700

    Create a Popover Full Screen Modal View

---
+++ KodecoSwiftUICookbook/CreatePopover.swift
@@ -0,0 +1,18 @@
+//
+//  CreatePopover.swift
+//  KodecoSwiftUICookbook
+//
+//  Created by Tatsuya Moriguchi on 6/29/24.
+//
+
+import SwiftUI
+
+struct CreatePopover: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreatePopover()
+}
---
+++ KodecoSwiftUICookbook/FullScreenModalView.swift
@@ -0,0 +1,18 @@
+//
+//  FullScreenModalView.swift
+//  KodecoSwiftUICookbook
+//
+//  Created by Tatsuya Moriguchi on 6/29/24.
+//
+
+import SwiftUI
+
+struct FullScreenModalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FullScreenModalView()
+}
