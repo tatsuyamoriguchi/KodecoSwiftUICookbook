1f7cef67a29593932d212247fba0312c995fd648
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2024-07-02 08:44:45 -0700

    Create a Video Player Customizing Audio and Video Playback

---
+++ KodecoSwiftUICookbook/CreateVideoPlayer.swift
@@ -0,0 +1,18 @@
+//
+//  CreateVideoPlayer.swift
+//  KodecoSwiftUICookbook
+//
+//  Created by Tatsuya Moriguchi on 7/2/24.
+//
+
+import SwiftUI
+
+struct CreateVideoPlayer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateVideoPlayer()
+}
---
+++ KodecoSwiftUICookbook/CustomizingAudioVideoPlayback.swift
@@ -0,0 +1,18 @@
+//
+//  CustomizingAudioVideoPlayback.swift
+//  KodecoSwiftUICookbook
+//
+//  Created by Tatsuya Moriguchi on 7/2/24.
+//
+
+import SwiftUI
+
+struct CustomizingAudioVideoPlayback: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomizingAudioVideoPlayback()
+}
