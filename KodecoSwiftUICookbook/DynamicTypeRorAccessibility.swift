d4be21e9a2dfe24b2004ba1bba8d1c6905d92295
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2024-07-07 12:06:04 -0700

    Dynamic Type Voice Over

---
+++ KodecoSwiftUICookbook/DynamicTypeRorAccessibility.swift
@@ -0,0 +1,18 @@
+//
+//  DynamicTypeRorAccessibility.swift
+//  KodecoSwiftUICookbook
+//
+//  Created by Tatsuya Moriguchi on 7/7/24.
+//
+
+import SwiftUI
+
+struct DynamicTypeRorAccessibility: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DynamicTypeRorAccessibility()
+}
---
+++ KodecoSwiftUICookbook/VoiceOver2.swift
@@ -0,0 +1,18 @@
+//
+//  VoiceOver2.swift
+//  KodecoSwiftUICookbook
+//
+//  Created by Tatsuya Moriguchi on 7/7/24.
+//
+
+import SwiftUI
+
+struct VoiceOver2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VoiceOver2()
+}
