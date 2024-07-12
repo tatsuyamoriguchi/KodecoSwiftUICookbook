eaa434eec6a34c45b60c7af94eedb455e5b163ba
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2024-07-12 10:06:36 -0700

    Localized strings

---
+++ KodecoSwiftUICookbook/LocalizedString.swift
@@ -0,0 +1,18 @@
+//
+//  LocalizedString.swift
+//  KodecoSwiftUICookbook
+//
+//  Created by Tatsuya Moriguchi on 7/12/24.
+//
+
+import SwiftUI
+
+struct LocalizedString: View {
+    var body: some View {
+        Text("hello_world")
+    }
+}
+
+#Preview {
+    LocalizedString()
+}
