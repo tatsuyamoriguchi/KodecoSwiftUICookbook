9373b8b21bceb66814da7c5308343ae02e31dd2e
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2024-07-15 10:20:00 -0700

    dynamic type for multilingual text

--- KodecoSwiftUICookbook/DynamicTypeforMultilingualText.swift
+++ KodecoSwiftUICookbook/DynamicTypeforMultilingualText.swift
@@ -8,8 +8,23 @@
 import SwiftUI
 
 struct DynamicTypeforMultilingualText: View {
+    @State private var textSize: CGFloat = 16
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            Slider(value: $textSize, in: 16...40, step: 1 )
+                .padding()
+                .labelsHidden()
+            
+            Text("A small black cat climbs a tall tree, looking around for prey.")
+                .font(.system(size: textSize))
+                .padding()
+            Text("Eine kleine schwarze Katze klettert auf einen hohen Baum und sucht nach Beute")
+                .font(.system(size: textSize))
+                .padding()
+            Spacer()
+            
+        }.padding()
     }
 }
 
